9d9f110ba01aaf7207803d34d126331a8a306359
Nithishkannan-2002 <160817029+Nithishkannan-2002@users.noreply.github.com>
2025-01-28 16:10:39 +0530

    Add files via upload

---
+++ calc.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class calc{
+    Scanner input = new Scanner(System.in);
+    int a = input.nextInt;
+    int b = input.nextInt;
+    int choice;
+    switch(){}
+    
+        
+    }
+   
+
